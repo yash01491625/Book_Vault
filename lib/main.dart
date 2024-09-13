7a54531b564dd3f74843d0b21304cb9ffec3c906
yash01491625 <yyashmalviya051@gmail.com>
2024-09-13 13:27:03 +0530

    Connected Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:book_vault/screens/userHomeScreen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:book_vault/screens/welcomeScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb) {
+    await Firebase.initializeApp(options: FirebaseOptions(
+        apiKey: "AIzaSyCGl9uHngFZE6sBAof7pZjdFTFqa4uhRAI",
+        authDomain: "book-vault-29cd7.firebaseapp.com",
+        projectId: "book-vault-29cd7",
+        storageBucket: "book-vault-29cd7.appspot.com",
+        messagingSenderId: "517870828399",
+        appId: "1:517870828399:web:2a0eeac2c929c1a5fb869a"
+      )
+    );
+  } else {
+    Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
